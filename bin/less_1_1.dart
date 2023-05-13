a5ff27386dee939ba0ee41a4b35a7937a91458e6
Timur2009KG <asanalievt93@gmail.com>
2023-05-13 20:06:45 +0300

    first commit

---
+++ bin/less_1_1.dart
@@ -0,0 +1,5 @@
+import 'package:less_1_1/less_1_1.dart' as less_1_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${less_1_1.calculate()}!');
+}
---
+++ test/less_1_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:less_1_1/less_1_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
